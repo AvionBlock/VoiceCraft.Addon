aca895eed9e19db2070f985a23baae2f4926fc67
SineVector241 <flyingbirdasuka1@gmail.com>
2025-07-22 12:18:31 +1000

    Rewrite utf8 encoder/decoder.

--- Core.McWss/packs/BP/scripts/main.js
+++ Core.McWss/packs/BP/scripts/main.js
@@ -1,6 +1,14 @@
 import { NetDataWriter } from "./network/NetDataWriter";
 import { NetDataReader } from "./network/NetDataReader";
+import UTF8 from "./utf8";
 
+const str = "⌨⌨⌨aa⌨";
+console.warn(str);
+const bytes = UTF8.getBytes(str);
+const string = UTF8.getString(bytes, 0, bytes.byteLength);
+console.warn(string);
+
+/*
 const writer = new NetDataWriter();
 const value = 0.235896286;
 const value2 = 0.23246623896286;
@@ -13,3 +21,4 @@ writer.putDouble(value2);
 writer.putString(value3);
 const reader = new NetDataReader(writer);
 console.warn(`Output: ${reader.getDouble()}, Output 2: ${reader.getDouble()}, Output 3: ${reader.getString()}`);
+*/
--- Core.McWss/packs/BP/scripts/utf8.js
+++ Core.McWss/packs/BP/scripts/utf8.js
@@ -1,119 +1,252 @@
-/**
- * @description Encodes a range of characters from a string into a byte array.
- * @param { String } s
- * @param { Number } charIndex
- * @param { Number } charCount
- * @param { ArrayBuffer } bytes
- * @param { Number } byteIndex
- * @returns { Number } The number of bytes that were successfully encoded.
- */
-function getBytes(s, charIndex, charCount, bytes, byteIndex) {
-  if (typeof s !== "string")
-    throw new TypeError("Parameter s is not a string!");
-  if (typeof charIndex !== "number")
-    throw new TypeError("Parameter charIndex is not a number!");
-  if (typeof charCount !== "number")
-    throw new TypeError("Parameter charCount is not a number!");
-  if (!(bytes instanceof ArrayBuffer))
-    throw new TypeError("Parameter bytes is not an instance of ArrayBuffer!");
-  if (typeof byteIndex !== "number")
-    throw new TypeError("Parameter byteIndex is not a number!");
-  if (s.length - charIndex < charCount)
-    throw new RangeError("Argument out of range!", { cause: "s" });
-  if (byteIndex > bytes.byteLength)
-    throw new RangeError(
-      "Byte Index must be less than or equal to bytes.byteLength!",
-      { cause: "byteIndex" }
-    );
+export default class UTF8 {
+  /**
+   * @description Encodes a string into an ArrayBuffer.
+   * @param { String } s
+   * @returns { ArrayBuffer | undefined }
+   */
+  static getBytes(s) {
+    if (typeof s !== "string")
+      throw new TypeError("Parameter s is not a string!");
 
-  charCount = Math.min(charCount, s.length);
-  let bytesEncoded = 0;
-  for (let i = charIndex; i < charIndex + charCount; i++) {
-    const charCode = s.charCodeAt(i);
+    const byteCount = this.getByteCount(s);
+    if(byteCount === 0) return undefined;
+
+    const bytes = new ArrayBuffer(byteCount);
+    this.setBytes(s, 0, s.length, bytes, 0);
+    return bytes;
+  }
+
+  /**
+   * @description Encodes a range of characters from a string into an ArrayBuffer.
+   * @param { String } s
+   * @param { Number } charIndex
+   * @param { Number } charCount
+   * @param { ArrayBuffer } bytes
+   * @param { Number } byteIndex
+   * @returns { Number } The number of bytes that were successfully encoded.
+   */
+  static setBytes(s, charIndex, charCount, bytes, byteIndex) {
+    if (typeof s !== "string")
+      throw new TypeError("Parameter s is not a string!");
+    if (typeof charIndex !== "number")
+      throw new TypeError("Parameter charIndex is not a number!");
+    if (typeof charCount !== "number")
+      throw new TypeError("Parameter charCount is not a number!");
+    if (!(bytes instanceof ArrayBuffer))
+      throw new TypeError("Parameter bytes is not an instance of ArrayBuffer!");
+    if (typeof byteIndex !== "number")
+      throw new TypeError("Parameter byteIndex is not a number!");
+    if (s.length - charIndex < charCount)
+      throw new RangeError("Argument out of range!", { cause: "s" });
+    if (byteIndex > bytes.byteLength)
+      throw new RangeError(
+        "Byte Index must be less than or equal to bytes.byteLength!",
+        { cause: "byteIndex" }
+      );
+
+    charCount = Math.min(charCount, s.length);
+    let bytesEncoded = 0;
+    for (let i = charIndex; i < charIndex + charCount; i++) {
+      const charCode = s.charCodeAt(i);
+      const byteCount = this.setBytesFromCharCode(charCode, bytes, byteIndex);
+      if(byteCount === undefined) continue;
+      bytesEncoded += byteCount;
+      byteIndex += byteCount;
+    }
+
+    return bytesEncoded;
+  }
+
+  /**
+   * @description Decodes a range of bytes from an ArrayBuffer into a string.
+   * @param { ArrayBuffer } bytes
+   * @param { Number } byteIndex
+   * @param { Number } count
+   * @returns { String } The decoded string.
+   */
+  static getString(bytes, index, count) {
+    if (!(bytes instanceof ArrayBuffer))
+      throw new TypeError("Parameter bytes is not an instance of ArrayBuffer!");
+    if (typeof index !== "number")
+      throw new TypeError("Parameter index is not a number!");
+    if (typeof count !== "number")
+      throw new TypeError("Parameter count is not a number!");
+    if (bytes.byteLength - index < count)
+      throw new RangeError("Count argument out of range!");
+
+    /** @type { Number[] } */
+    const charCodes = [];
+    const maxIndex = count + index;
+    while (index < maxIndex) {
+      const charCode = this.getCharCodeFromBytes(bytes, index);
+      if (charCode !== undefined) {
+        charCodes.push(charCode);
+        index += this.getByteCountFromCharCode(charCode); //We know it's a valid utf8 character.
+        continue;
+      }
+
+      index++;
+    }
+
+    return String.fromCharCode.apply(null, charCodes);
+  }
+
+  //#region Byte Count Stuff
+  /**
+   * @description Calculates the exact number of bytes required to encode the string.
+   * @param { String } chars
+   * @returns { Number } The number of bytes that is required to encode the string.
+   */
+  static getByteCount(chars) {
+    if (typeof chars !== "string")
+      throw new TypeError("Parameter chars is not a string!");
+
+    let charCount = 0;
+    for (let i = 0; i < chars.length; i++) {
+      const byteCount = this.getByteCountFromCharCode(chars.charCodeAt(i));
+      if (byteCount === undefined) continue;
+      charCount += byteCount;
+    }
+
+    return charCount;
+  }
+  //#endregion
+
+  /**
+   * @description Calculates the maximum number of bytes produced by encoding the specified number of characters.
+   * @param { Number } charCount
+   * @returns { Number } The maximum possible number of bytes required to encode.
+   */
+  static getMaxByteCount(charCount) {
+    if (typeof charCount !== "number")
+      throw new TypeError("Parameter charCount is not a number!");
+
+    return (charCount + 1) * 3;
+  }
+
+  //#region Charcode Stuff
+  /**
+   * @description Calculates the required numbers of bytes to encode the specified char code.
+   * @param { Number } charCode
+   * @returns { Number | undefined }
+   */
+  static getByteCountFromCharCode(charCode) {
+    if (typeof charCode !== "number")
+      throw new TypeError("Parameter charCode is not a number!");
+
     if (charCode <= 0x7f) {
-      // 0XXX XXXX 1 byte
-      bytes[byteIndex++] = charCode;
-      bytesEncoded++;
+      return 1;
     } else if (charCode <= 0x7ff) {
-      // 110X XXXX 2 bytes
-      bytes[byteIndex++] = 0xc0 | (charCode >> 6);
-      bytes[byteIndex++] = 0x80 | (charCode & 0x3f);
-      bytesEncoded += 2;
+      return 2;
     } else if (charCode <= 0xffff) {
-      // 1110 XXXX 3 bytes
-      bytes[byteIndex++] = 0xe0 | (charCode >> 12);
-      bytes[byteIndex++] = 0x80 | ((charCode >> 6) & 0x3f);
-      bytes[byteIndex++] = 0x80 | (charCode & 0x3f);
-      bytesEncoded += 3;
+      return 3;
     }
+    return undefined;
   }
 
-  return bytesEncoded;
-}
+  /**
+   * @description Get's an ArrayBuffer with the encoded bytes for the specified charcode.
+   * @param { Number } charCode
+   * @returns { ArrayBuffer | undefined }
+   */
+  static getBytesFromCharCode(charCode) {
+    if (typeof charCode !== "number")
+      throw new TypeError("Parameter charCode is not a number!");
 
-/**
- * @description Decodes a range of bytes from a byte array into a string.
- * @param { ArrayBuffer } bytes
- * @param { Number } byteIndex
- * @param { Number } count
- * @returns { String } The decoded string.
- */
-function getString(bytes, index, count) {
-  if (!(bytes instanceof ArrayBuffer))
-    throw new TypeError("Parameter bytes is not an instance of ArrayBuffer!");
-  if (typeof index !== "number")
-    throw new TypeError("Parameter index is not a number!");
-  if (typeof count !== "number")
-    throw new TypeError("Parameter count is not a number!");
-  if (bytes.byteLength - index < count)
-    throw new RangeError("Argument out of range!");
+    const byteCount = this.getByteCountFromCharCode(charCode);
+    if (byteCount === undefined) return undefined;
 
-  var string = "";
-  const maxIndex = count + index;
-  while (index < maxIndex) {
-    const byte = bytes[index++];
+    const bytes = new ArrayBuffer(byteCount);
+    this.setBytesFromCharCode(charCode, bytes, 0);
+    return bytes;
+  }
+
+  /**
+   * @description Sets an ArrayBuffer with the encoded bytes for the specified charcode.
+   * @param { Number } charCode
+   * @param { ArrayBuffer } bytes
+   * @param { Number } index
+   * @returns { Number | undefined } The number of bytes encoded.
+   */
+  static setBytesFromCharCode(charCode, bytes, index) {
+    if (typeof charCode !== "number")
+      throw new TypeError("Parameter charCode is not a number!");
+    if (!(bytes instanceof ArrayBuffer))
+      throw new TypeError("Parameter bytes is not an instance of ArrayBuffer!");
+    if (typeof index !== "number")
+      throw new TypeError("Parameter index is not a number!");
+
+    let byteCount = this.getByteCountFromCharCode(charCode);
+    switch (byteCount) {
+      case 1:
+        if (bytes.byteLength < index) {
+          byteCount = undefined;
+          break;
+        }
+
+        bytes[index] = charCode;
+        break;
+      case 2:
+        if (bytes.byteLength < index + 1) {
+          byteCount = undefined;
+          break;
+        }
+
+        bytes[index++] = 0xc0 | (charCode >> 6);
+        bytes[index] = 0x80 | (charCode & 0x3f);
+        break;
+      case 3:
+        if (bytes.byteLength < index + 2) {
+          byteCount = undefined;
+          break;
+        }
+
+        bytes[index++] = 0xe0 | (charCode >> 12);
+        bytes[index++] = 0x80 | ((charCode >> 6) & 0x3f);
+        bytes[index] = 0x80 | (charCode & 0x3f);
+        break;
+    }
+
+    return byteCount;
+  }
+
+  /**
+   * @description Get's a charcode from the specified ArrayBuffer at index.
+   * @param { ArrayBuffer } bytes
+   * @param { Number } index
+   * @returns { Number | undefined }
+   */
+  static getCharCodeFromBytes(bytes, index) {
+    if (!(bytes instanceof ArrayBuffer))
+      throw new TypeError("Parameter bytes is not an instance of ArrayBuffer!");
+    if (typeof index !== "number")
+      throw new TypeError("Parameter index is not a number!");
+    if (bytes.byteLength < index)
+      throw new RangeError("Index argument out of range!");
+
+    const byte = bytes[index];
     if ((byte & 0x80) === 0) {
-      string = string.concat(String.fromCharCode(byte));
+      return byte;
     } else if ((byte & 0xe0) == 0xc0) {
-      const byte2 = bytes[index++];
-      string = string.concat(
-        String.fromCharCode(((byte & 0x1f) << 6) | (byte2 & 0x3f))
-      );
+      const byte2 = bytes[++index];
+      return ((byte & 0x1f) << 6) | (byte2 & 0x3f);
     } else if ((byte & 0xf0) == 0xe0) {
-      const byte2 = bytes[index++];
-      const byte3 = bytes[index++];
-      string = string.concat(
-        String.fromCharCode(
-          ((byte & 0x0f) << 12) | ((byte2 & 0x3f) << 6) | (byte3 & 0x3f)
-        )
-      );
+      const byte2 = bytes[++index];
+      const byte3 = bytes[++index];
+      return ((byte & 0x0f) << 12) | ((byte2 & 0x3f) << 6) | (byte3 & 0x3f);
     } else if ((byte & 0xf8) == 0xf0) {
-      const byte2 = bytes[index++];
-      const byte3 = bytes[index++];
-      const byte4 = bytes[index++];
-      string = string.concat(
-        String.fromCharCode(
-          ((byte & 0x0f) << 18) |
-            ((byte2 & 0x3f) << 12) |
-            ((byte3 & 0x3f) << 6) |
-            (byte4 & 0x3f)
-        )
+      const byte2 = bytes[++index];
+      const byte3 = bytes[++index];
+      const byte4 = bytes[++index];
+      return (
+        ((byte & 0x0f) << 18) |
+        ((byte2 & 0x3f) << 12) |
+        ((byte3 & 0x3f) << 6) |
+        (byte4 & 0x3f)
       );
     }
+
+    return undefined;
   }
-
-  return string;
+  //#endregion
 }
-
-/**
- * @description Calculates the maximum number of bytes produced by encoding the specified number of characters.
- * @param { Number } charCount
- * @returns { Number } The maximum possible number of bytes required to encode.
- */
-function getMaxByteCount(charCount) {
-  const num1 = charCount + 1;
-  const num2 = num1 * 3;
-  return num2;
-}
-
-export { getBytes, getString, getMaxByteCount };
